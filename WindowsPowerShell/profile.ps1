5d3d0b75a2aca950bb518820713c778294647799
albertdev <albert.devs@gmail.com>
2020-05-16 12:36:12 +0200

    Powershell: function to pretify all JSON files, function to run nuget / npm

--- WindowsPowerShell/profile.ps1
+++ WindowsPowerShell/profile.ps1
@@ -387,3 +387,116 @@ function GitContains ([string]$Needle, [string]$BranchOrCommit)
         }
     }
 }
+# Restores Nuget and npm packages
+function Add-ProjectPackages ()
+{
+    $solutions = Get-ChildItem -Recurse "*.sln"
+    $npmFiles = Get-ChildItem -Recurse "package.json"
+    $currentDir = Get-Location
+    try {
+        foreach ($solution in $solutions) {
+            Set-Location (Split-Path -Parent $solution)
+            nuget restore
+        }
+        foreach ($npmFile in $npmFiles) {
+            # Ignore package.json in earlier downloaded node modules
+            if (([string]$npmFile).Contains("node_modules")) {
+                continue
+            }
+            Set-Location (Split-Path -Parent $npmFile)
+            npm run-script get-frontend
+        }
+    } finally {
+        Set-Location $currentDir
+    }
+}
+# Finds all JSON files in subdirectories and "pretifies" them. It is assumed that they used the UTF8-no-BOM encoding.
+Function ReformatJson() {
+    [CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact = 'high')]
+    param()
+
+    $jsonFiles = Get-ChildItem -Recurse "*.json"
+    $utf8NoBom = New-Object System.Text.UTF8Encoding $false
+
+    if ($PSCmdlet.ShouldProcess($jsonFiles, "Reformatting $($jsonFiles.Length) JSON files")) {
+        foreach ($jsonFile in $jsonFiles) {
+            Write-Output "Reformatting $(Resolve-Path $jsonFile)"
+
+
+
+            $prettyContent = Get-Content -Encoding UTF8 -Raw $jsonFile | ConvertFrom-Json | ConvertTo-Json -Depth 100 | Format-Json
+            # We explicitly convert to have no BOM, hence Byte encoding to write a byte array
+            Set-Content -Path $jsonFile -Encoding Byte -Value $utf8NoBom.GetBytes($prettyContent)
+        }
+    }
+}
+# Taken from https://stackoverflow.com/a/56324939
+function Format-Json {
+    <#
+    .SYNOPSIS
+        Prettifies JSON output.
+    .DESCRIPTION
+        Reformats a JSON string so the output looks better than what ConvertTo-Json outputs.
+    .PARAMETER Json
+        Required: [string] The JSON text to prettify.
+    .PARAMETER Minify
+        Optional: Returns the json string compressed.
+    .PARAMETER Indentation
+        Optional: The number of spaces (1..1024) to use for indentation. Defaults to 4.
+    .PARAMETER AsArray
+        Optional: If set, the output will be in the form of a string array, otherwise a single string is output.
+    .EXAMPLE
+        $json | ConvertTo-Json  | Format-Json -Indentation 2
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'Prettify')]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [string]$Json,
+
+        [Parameter(ParameterSetName = 'Minify')]
+        [switch]$Minify,
+
+        [Parameter(ParameterSetName = 'Prettify')]
+        [ValidateRange(1, 1024)]
+        [int]$Indentation = 4,
+
+        [Parameter(ParameterSetName = 'Prettify')]
+        [switch]$AsArray
+    )
+
+    if ($PSCmdlet.ParameterSetName -eq 'Minify') {
+        return ($Json | ConvertFrom-Json) | ConvertTo-Json -Depth 100 -Compress
+    }
+
+    # If the input JSON text has been created with ConvertTo-Json -Compress
+    # then we first need to reconvert it without compression
+    if ($Json -notmatch '\r?\n') {
+        $Json = ($Json | ConvertFrom-Json) | ConvertTo-Json -Depth 100
+    }
+
+    $indent = 0
+    $regexUnlessQuoted = '(?=([^"]*"[^"]*")*[^"]*$)'
+
+    $result = $Json -split '\r?\n' |
+        ForEach-Object {
+            # If the line contains a ] or } character, 
+            # we need to decrement the indentation level unless it is inside quotes.
+            if ($_ -match "[}\]]$regexUnlessQuoted") {
+                $indent = [Math]::Max($indent - $Indentation, 0)
+            }
+
+            # Replace all colon-space combinations by ": " unless it is inside quotes.
+            $line = (' ' * $indent) + ($_.TrimStart() -replace ":\s+$regexUnlessQuoted", ': ')
+
+            # If the line contains a [ or { character, 
+            # we need to increment the indentation level unless it is inside quotes.
+            if ($_ -match "[\{\[]$regexUnlessQuoted") {
+                $indent += $Indentation
+            }
+
+            $line
+        }
+
+    if ($AsArray) { return $result }
+    return $result -Join [Environment]::NewLine
+}
