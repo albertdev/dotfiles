a543182d25688c631e1ac3b5b75ad61865755d95
albertdev <albert.devs@gmail.com>
2021-01-09 21:11:17 +0100

    Homemaker: link custom bin scripts / exes

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,12 @@
-.\homemaker -dest (Resolve-Path "$env:HOMEDRIVE$env:HOMEPATH") -verbose -variant windows homemaker.toml (Resolve-Path .)
+$commands = @(Get-Command "homemaker.exe" -ErrorAction Continue)
+if (-not ($commands)) {
+	$commands = @(Join-Path -Resolve "$env:HOMEDRIVE$env:HOMEPATH" "bin/homemaker.exe")
+    if (-not ($commands)) {
+        $commands = @(Resolve-Path ".\homemaker.exe")
+    }
+    if (-not ($commands)) {
+        throw "No homemaker executable found!"
+    }
+}
+$homemakerExe = ($commands | Select-Object -First 1)
+& $homemakerExe -dest (Resolve-Path "$env:HOMEDRIVE$env:HOMEPATH") -verbose -variant windows homemaker.toml (Resolve-Path .)
