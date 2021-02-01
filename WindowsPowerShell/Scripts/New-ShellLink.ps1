075b9aee53c2b715478dbada1e75ae5fa753627c
albertdev <albert.devs@gmail.com>
2021-02-01 13:23:46 +0100

    Powershell: Add script to create Windows shortcuts (.LNK)

---
+++ WindowsPowerShell/Scripts/New-ShellLink.ps1
@@ -0,0 +1,77 @@
+[CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact = 'high')]
+param (
+
+    [parameter(Mandatory=$true)]
+    [string] $LinkOutputPath,
+
+    [parameter(Mandatory=$true)]
+    [string] $TargetPath,
+
+    [parameter()]
+    [string] $Arguments,
+
+    [parameter()]
+    [string] $StartPath,
+
+    [parameter()]
+    [Switch] $Force
+)
+
+## An improved "Create Shell Shortcut" script. PSCX might have New-Shortcut but it doesn't allow to customize the link.
+
+if ($Force) {
+    $ConfirmPreference = 'None'
+}
+
+# Check the target path exists
+if (-not (Test-Path $TargetPath)) {
+    # Maybe targetpath uses classic CMD environnmental variables
+    $expandedPath = [System.Environment]::ExpandEnvironmentVariables($TargetPath)
+    if (-not (Test-Path $expandedPath)) {
+        throw "TargetPath [$TargetPath] does not point to an existing executable or script"
+    }
+}
+
+# Check the start path exists (if indicated)
+if ($StartPath) {
+    if (-not (Test-Path $StartPath)) {
+        # Maybe startpath uses classic CMD environnmental variables
+        $expandedPath = [System.Environment]::ExpandEnvironmentVariables($StartPath)
+        if (-not (Test-Path $expandedPath)) {
+            throw "StartPath [$StartPath] does not point to an existing directory"
+        }
+    }
+}
+
+# Validate that the link doesn't exist yet. If it does, prepare a confirmation prompt (if the user's preference allows it)
+if (Test-Path $LinkOutputPath) {
+    $LinkOutputPath = Resolve-Path $LinkOutputPath
+    if ( -not ($PSCmdlet.ShouldProcess($LinkOutputPath, "Overwrite existing link file"))) {
+        exit
+    }
+    Write-Warning "Overwriting link file $LinkOutputPath"
+}
+
+$objShell = New-Object -ComObject WScript.Shell
+$lnk = $objShell.CreateShortcut($LinkOutputPath)
+
+if (-not $lnk) {
+    throw "Failed to create shortcut $LinkOutputPath"
+}
+
+
+$lnk.TargetPath = $TargetPath
+
+if ($Arguments) {
+    $lnk.Arguments = $Arguments
+}
+
+if ($StartPath) {
+    $lnk.WorkingDirectory = $StartPath
+}
+
+$lnk.Save()
+
+# Cargo cult code
+$lnk = $null
+$objShell = $null
