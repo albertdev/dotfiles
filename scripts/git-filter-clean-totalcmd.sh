0c8b1b1b927e733810ec9ffe666a9c3a1628d435
albertdev <albert.devs@gmail.com>
2022-04-14 01:40:20 +0200

    TC: Fix git smudge/clean filter for *nix versions of Gawk
    Git comes with its own version of GNU awk, and that version wants to
    replace newlines with a LF newline character, whereas the original file
    uses CR/LF newline characters.
    
    Then depending on the Windows shell one could get a Windows version of
    Gawk which would yield a correct result, yet fail to work when used in a
    Git Bash shell.
    
    Hard-coding the gawk path seems to do the trick. If this fails on common
    Linux distros then /usr/bin/gawk might need to be used instead.

--- scripts/git-filter-clean-totalcmd.sh
+++ scripts/git-filter-clean-totalcmd.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 # Reset particular ini values to a fixed state.
-# Sadly GNU sed seems to turn the file into a line-feed delimited one rather than CRLF
-#sed 's/^\(firstmnu\)=.*/\1=2211/;s/^\(QuickSearchAutoFilter\)=.*/\1=0/;s/^\(LastSearchOptions\)=.*/\1=96/'
-#gawk -F= -- '$1=="firstmnu"{print $1"=2211";next}$1=="QuickSearchAutoFilter"{print $1"=0";next}$1=="LastSearchOptions"{print $1"=96";next}1{print}'
-gawk -F= -v OFS='=' -- '$1=="firstmnu"{$2="2211"}$1=="QuickSearchAutoFilter"{$2="0"}$1=="LastSearchOptions"{$2="96"}{print}'
+# Hard-coded gawk path is to make sure no that Windows version of gawk gets used - it turns the \n into another pair of \r\n
+/bin/gawk -F= -v OFS='=' -v ORS='\r\n' -- '$1=="firstmnu"{$2="2211"}$1=="QuickSearchAutoFilter"{$2="0"}$1=="LastSearchOptions"{$2="96"}{print}'
