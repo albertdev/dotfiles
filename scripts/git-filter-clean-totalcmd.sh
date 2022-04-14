fb6e15031277e2cf4d5cf9d220e1b86682460009
Bert Jacobs <albert.devs@gmail.com>
2022-04-14 11:09:41 +0200

    TC: Use AES-256 for zip encryption, default checksum is MD5, template
    Also update the filter to ignore changes to lastmd5 from now on.
    
    Re-enable the local dirs template now that my Homemaker PR got accepted.

--- scripts/git-filter-clean-totalcmd.sh
+++ scripts/git-filter-clean-totalcmd.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Reset particular ini values to a fixed state.
 # Hard-coded gawk path is to make sure no that Windows version of gawk gets used - it turns the \n into another pair of \r\n
-/bin/gawk -F= -v OFS='=' -v ORS='\r\n' -- '$1=="firstmnu"{$2="2211"}$1=="QuickSearchAutoFilter"{$2="0"}$1=="LastSearchOptions"{$2="96"}{print}'
+/bin/gawk -F= -v OFS='=' -v ORS='\r\n' -- \
+  '$1=="firstmnu"{$2="2211"}$1=="QuickSearchAutoFilter"{$2="0"}$1=="LastSearchOptions"{$2="96"}$1=="lastmd5"{$2="1"}{print}'
