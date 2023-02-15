2f4da261f4694d21acee0706f3731e379a4a8ec9
albertdev <albert.devs@gmail.com>
2023-02-15 13:14:46 +0100

    TC: Update clean filter so that it excludes more fields

--- scripts/git-filter-clean-totalcmd.sh
+++ scripts/git-filter-clean-totalcmd.sh
@@ -2,4 +2,4 @@
 # Reset particular ini values to a fixed state.
 # Hard-coded gawk path is to make sure no that Windows version of gawk gets used - it turns the \n into another pair of \r\n
 /bin/gawk -F= -v OFS='=' -v ORS='\r\n' -- \
-  '$1=="firstmnu"{$2="2211"}$1=="QuickSearchAutoFilter"{$2="0"}$1=="LastSearchOptions"{$2="96"}$1=="lastmd5"{$2="1"}{print}'
+  '$1=="firstmnu"{$2="2211"}$1=="QuickSearchAutoFilter"{$2="0"}$1=="LastSearchOptions"{$2="96"}$1=="lastmd5"{$2="1"}$1=="test"{$2="67"}$1=="LastUsedPacker64"{$2="1"}$1=="LastUsedPacker"{$2="1"}{print}'
